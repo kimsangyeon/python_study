ea5dbf89464fa0d36e73ff1673d1511b0b7b7c90
yeon <dho113@naver.com>
2017-09-10 18:25:06 +0900

    python_study3
    - 리스트 함수 사용

--- pytho_study3/python_study3.py
+++ pytho_study3/python_study3.py
@@ -4,7 +4,7 @@
 list = 배열 (array)
 """
 
-list = ['sun', 'mon', 'tue', 'wed', 'thu', 'fir', 'sat']
+list = ['sun', 'mon', 'tue', 'wed', 'thu', 'fir']
 print("sunday : ", list[0])
 
 list[0] = "sunday"
@@ -12,10 +12,30 @@ print("일요일:", list[0])
 
 print(list[1:3])
 
-days = ['mon', 'tue', 'wed']
-week = [days, list]
-print(week)nums = ['one', 'two', 'three']
+nums = ['one', 'two', 'three']
 week = [nums, list]
 print(week)
 print(week[1][1])
 print(week[0][0])
+
+# append 맨뒤에 추가
+list.append('sat')
+print(week)
+
+# insert 원하는 index에 추가
+list.insert(1, 'holiday')
+print(week)
+
+# remove 데이터 삭제
+list.remove('mon')
+print(week)
+
+# sort 정렬
+list.sort();
+print(week)
+
+# reverse 뒤집기
+list.reverse();
+print(week)
+
+
