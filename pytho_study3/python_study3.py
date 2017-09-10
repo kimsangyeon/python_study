f228e4da98edd8aa12a02f55b9b1aeefd9ba88b8
yeon <dho113@naver.com>
2017-09-10 18:23:08 +0900

    python_study3
    - 리스트 할당 및 2차원 리스트 만들기

--- pytho_study3/python_study3.py
+++ pytho_study3/python_study3.py
@@ -14,4 +14,8 @@ print(list[1:3])
 
 days = ['mon', 'tue', 'wed']
 week = [days, list]
+print(week)nums = ['one', 'two', 'three']
+week = [nums, list]
 print(week)
+print(week[1][1])
+print(week[0][0])
