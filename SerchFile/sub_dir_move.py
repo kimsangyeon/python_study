dfce9c3a0cc942b37216d9b79614a23e091f2e3e
yeon <dho113@naver.com>
2018-01-27 09:52:42 +0900

    python_study/sub directory move
    - os system이용하여 파일 이동 시키기

--- SerchFile/sub_dir_move.py
+++ SerchFile/sub_dir_move.py
@@ -1,9 +1,10 @@
-import shutil
+import os, shutil
 
 fileName = 'test.sangyeon'
 src = '/Users/sangyeon/python_study/SerchFile/'
 testDir = '/Users/sangyeon/python_study/SerchFile/test/'
 
-shutil.move(src + fileName, testDir +fileName)
+# shutil.move(src + fileName, testDir +fileName)
 
-
+command = "mv " + src + fileName + " " + testDir
+os.system(command)
