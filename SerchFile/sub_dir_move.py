1b97659f710015849779ab9bf1658af4337d668b
yeon <dho113@naver.com>
2018-02-04 21:48:54 +0900

    python_study/sub directory move
    - sub_dir_move.py 함수화

--- SerchFile/sub_dir_move.py
+++ SerchFile/sub_dir_move.py
@@ -1,12 +1,12 @@
 import os, shutil
 
-fileName = 'test.sangyeon'
-src = '/Users/sangyeon/python_study/SerchFile/'
-testDir = '/Users/sangyeon/python_study/SerchFile/test/'
+# fileName = 'test.sangyeon'
+# src = '/Users/sangyeon/python_study/SerchFile/'
+# testDir = '/Users/sangyeon/python_study/SerchFile/test/'
 
-# shutil.move(src + fileName, testDir +fileName)
-
-# command = "mv " + src + fileName + " " + testDir
-# os.system(command)
-
-os.rename(src + fileName, testDir + fileName)
+def mv(src, fileName, moveDir):
+    # shutil.move(src + fileName, testDir +fileName)
+    # os.rename(src + fileName, testDir + fileName)
+    
+    command = "mv " + src + fileName + " " + moveDir
+    os.system(command)
