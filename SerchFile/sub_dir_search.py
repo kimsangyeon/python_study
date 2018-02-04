ffc52f3fc92647674cafc608d4c385489af852da
yeon <dho113@naver.com>
2018-02-04 21:50:01 +0900

    python_study/sub directory move
    - sub_dir_move.py import 하여 file 이동시키는 로직 구현

--- SerchFile/sub_dir_search.py
+++ SerchFile/sub_dir_search.py
@@ -1,20 +1,27 @@
 # Sub Directory Search File
 import sys, os
 from errno import EACCES, EPERM, ENOENT
+from sub_dir_move import mv
 
-def search(dirName):
+testDir = '/Users/sangyeon/python_study/SerchFile/test'
+
+def search(src):
     try:
-        fileNames = os.listdir(dirName)
+        fileNames = os.listdir(src)
 
         for fileName in fileNames:
-            filePath = os.path.join(dirName, fileName)
+            filePath = os.path.join(src, fileName)
 
             if os.path.isdir(filePath):
                 search(filePath)
             else:
                 ext = os.path.splitext(filePath)[1]
-                if ext == '.py':
-                    print(filePath)
+                if ext == '.txt':
+                    print 'Before Path: %s' % src
+                    print 'Move Path: %s ' % testDir
+                    print 'file: %s' % fileName
+                    mv(src, fileName, testDir)
+                    print '\n'
     except (IOError, OSError) as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         fileName = os.getcwd() + "/" + os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
@@ -41,11 +48,11 @@ def print_error_message(e, file_name):
         print("PermissionError error({0}): {1} for: {2}".format(e.errno, e.strerror, file_name))
     #FileNotFoundError
     elif e.errno==ENOENT:
-        print("FileNotFoundError error({0}): {1} as: {2}".format(e.errno, e.strerror, full_name))
+        print("FileNotFoundError error({0}): {1} as: {2}".format(e.errno, e.strerror, file_name))
     elif IOError:
         print("I/O error({0}): {1} as: {2}".format(e.errno, e.strerror, file_name))
     elif OSError:
         print("OS error({0}): {1} as: {2}".format(e.errno, e.strerror, file_name))
 
 
-search("/Users/sangyeon/python_study")
+search("/Users/sangyeon/python_study/SerchFile/")
