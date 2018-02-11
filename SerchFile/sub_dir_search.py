40f2929f3f58801c233591a0a065ed71bd9faf2e
yeon <dho113@naver.com>
2018-02-11 22:07:10 +0900

    python_study/ 문서 포멧별 mv 동작 추가

--- SerchFile/sub_dir_search.py
+++ SerchFile/sub_dir_search.py
@@ -4,6 +4,11 @@ from errno import EACCES, EPERM, ENOENT
 from sub_dir_move import mv
 
 testDir = '/Users/sangyeon/python_study/SerchFile/test'
+docDir = '/Users/sangyeon/python_study/SerchFile/doc'
+docxDir = '/Users/sangyeon/python_study/SerchFile/docx'
+pptDir = '/Users/sangyeon/python_study/SerchFile/ppt'
+pptxDir = '/Users/sangyeon/python_study/SerchFile/pptx'
+xlsxDir = '/Users/sangyeon/python_study/SerchFile/xlsx'
 
 def search(src):
     try:
@@ -21,6 +26,36 @@ def search(src):
                     print 'Move Path: %s ' % testDir
                     print 'file: %s' % fileName
                     mv(src, fileName, testDir)
+                    print '\n'
+                elif ext == '.doc':
+                    print 'Before Path: %s' % src
+                    print 'Move Path: %s ' % docDir
+                    print 'file: %s' % fileName
+                    mv(src, fileName, docDir)
+                    print '\n'
+                elif ext == '.docx':
+                    print 'Before Path: %s' % src
+                    print 'Move Path: %s ' % docxDir
+                    print 'file: %s' % fileName
+                    mv(src, fileName, docxDir)
+                    print '\n'
+                elif ext == '.ppt':
+                    print 'Before Path: %s' % src
+                    print 'Move Path: %s ' % pptDir
+                    print 'file: %s' % fileName
+                    mv(src, fileName, pptDir)
+                    print '\n'
+                elif ext == '.pptx':
+                    print 'Before Path: %s' % src
+                    print 'Move Path: %s ' % pptxDir
+                    print 'file: %s' % fileName
+                    mv(src, fileName, pptxDir)
+                    print '\n'
+                elif ext == '.xlsx':
+                    print 'Before Path: %s' % src
+                    print 'Move Path: %s ' % xlsxDir
+                    print 'file: %s' % fileName
+                    mv(src, fileName, xlsxDir)
                     print '\n'
     except (IOError, OSError) as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
