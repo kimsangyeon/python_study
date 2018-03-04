eeb0cff46f54f5f04ccfd46d01560fd1e682e04e
yeon <dho113@naver.com>
2018-03-04 22:39:28 +0900

    python_study/ python game
    - pygame 사용 주석 추가

--- pygame/game.py
+++ pygame/game.py
@@ -6,7 +6,11 @@ pygame.display.set_caption('파이게임')
 finish = False
 
 while not finish:
+    # 이벤트를 처리하는 부분 -> 키보드, 마우스 등의 이벤트 처리 코드가 들어감
     for event in pygame.event.get():
+        # 윈도우의 닫기 버튼이 눌렸을 때, 프로그램을 종료하도록 처리
         if event.type == pygame.QUIT:
             finish = True
         pygame.display.flip()
+    # 게임의 상태를 업데이트하는 부분
+    # 게임의 상태를 화면에 그려주는 부분 -> 화면을 지우고, 그리고, 업데이트하는 코드가 들어감
