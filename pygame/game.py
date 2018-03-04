5280b1e80a3d8a6cbd20f281f90078407fbda950
yeon <dho113@naver.com>
2018-03-04 21:49:50 +0900

    python_study/ python game
    - pygame 사용하여 윈도우 창 띄우기

--- pygame/game.py
+++ pygame/game.py
@@ -1,3 +1,12 @@
 import pygame
-t = pygame.init()
-print(t)
+pygame.init()
+
+screen = pygame.display.set_mode((400,300))
+pygame.display.set_caption('파이게임')
+finish = False
+
+while not finish:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finish = True
+        pygame.display.flip()
