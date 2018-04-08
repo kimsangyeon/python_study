8b68ce2dded906bd20dbce60784afa0133e85c8d
Kim Sangyeon <dho113@naver.com>
2018-04-08 15:47:33 +0900

    rabbit_game/ 마우스 포지션에 따라 캐릭터 회전 시키기 동작

--- rabbit_game/game.py
+++ rabbit_game/game.py
@@ -1,6 +1,7 @@
 # https://www.raywenderlich.com/24252/beginning-game-programming-for-teens-with-python
 
 # 1 - Import library
+import math
 import pygame
 from pygame.locals import *
 
@@ -9,11 +10,21 @@ pygame.init()
 width, height = 640, 480
 screen=pygame.display.set_mode((width, height))
 
+# FPS
+FPS = 60
+fpsClock = pygame.time.Clock()
+
 # 3 - Load images
 player = pygame.image.load("resources/images/dude.png")
 grass = pygame.image.load("resources/images/grass.png")
 castle = pygame.image.load("resources/images/castle.png")
+arrow = pygame.image.load("resources/images/bullet.png")
 
+# key input check
+keys = [False, False, False, False]
+
+#player place(bunny)
+playerpos = [100, 100]
 
 running = 1
 # 4 - keep looping through
@@ -31,9 +42,14 @@ while running:
     screen.blit(castle,(0,345 ))
 
     # 6 - draw the screen elements
-    screen.blit(player, (100,100))
+    position = pygame.mouse.get_pos()
+    angle = math.atan2(position[1]-(playerpos[1]+32),position[0]-(playerpos[0]+26))
+    playerrot = pygame.transform.rotate(player, 360-angle*57.29)
+    playerpos1 = (playerpos[0]-playerrot.get_rect().width/2, playerpos[1]-playerrot.get_rect().height/2)
+    screen.blit(playerrot, playerpos1)
     # 7 - update the screen
     pygame.display.flip()
+    fpsClock.tick(FPS)
     # 8 - loop through the events
     for event in pygame.event.get():
         # check if the event is the X button
@@ -41,3 +57,33 @@ while running:
             # if it is quit the game
             pygame.quit()
             exit(0)
+        # key down event
+        if event.type == pygame.KEYDOWN:
+            if event.key==K_w:
+                keys[0]=True
+            elif event.key==K_a:
+                keys[1]=True
+            elif event.key==K_s:
+                keys[2]=True
+            elif event.key==K_d:
+                keys[3]=True
+        # key up event
+        if event.type == pygame.KEYUP:
+            if event.key==pygame.K_w:
+                keys[0]=False
+            elif event.key==pygame.K_a:
+                keys[1]=False
+            elif event.key==pygame.K_s:
+                keys[2]=False
+            elif event.key==pygame.K_d:
+                keys[3]=False
+    # player pos movement
+    if keys[0]:
+        playerpos[1]-=5
+    elif keys[2]:
+        playerpos[1]+=5
+    if keys[1]:
+        playerpos[0]-=5
+    elif keys[3]:
+        playerpos[0]+=5
+
