6321ba6780fef91e8240a768be2d31739936b359
Kim Sangyeon <dho113@naver.com>
2018-04-08 11:42:47 +0900

    rabbit_game/ 잔디그리기 로직 추가

--- rabbit_game/game.py
+++ rabbit_game/game.py
@@ -11,11 +11,21 @@ screen=pygame.display.set_mode((width, height))
 
 # 3 - Load images
 player = pygame.image.load("resources/images/dude.png")
+grass = pygame.image.load("resources/images/grass.png")
+castle = pygame.image.load("resources/images/castle.png")
 
+
+running = 1
 # 4 - keep looping through
-while 1:
+while running:
     # 5 - clear the screen before drawing it again
     screen.fill(0)
+
+    #grass paint
+    for x in range(int(width/grass.get_width()+1)):
+        for y in range(int(height/grass.get_height()+1)):
+            screen.blit(grass,(x*100,y*100))
+
     # 6 - draw the screen elements
     screen.blit(player, (100,100))
     # 7 - update the screen
