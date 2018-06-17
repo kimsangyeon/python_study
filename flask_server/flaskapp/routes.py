af84120658bed6ffd41253fa8acb8a611fe2bd70
Kim Sangyeon <dho113@naver.com>
2018-06-17 23:38:53 +0900

    python flask_server 만들기
    - post getSerizlizedData api 만들기

--- flask_server/flaskapp/routes.py
+++ flask_server/flaskapp/routes.py
@@ -1,12 +1,24 @@
 # routes
 # -- coding: utf-8 --
+import os
+import sys
 from flask import Flask, request, render_template
+from werkzeug import secure_filename
 
 app = Flask(__name__)
+os.path.expanduser('~\\document')
 
 @app.route("/")
 def index():
+    print(sys.prefix)
     return render_template("index.html")
+
+@app.route("/getSerializedPbData", methods = ['POST', 'GET'])
+def getSerializedPbData():
+    if request.method == 'POST':
+        f = request.files['docFile']
+        f.save(os.path.abspath('~/') + secure_filename(f.filename))
+
 
 if __name__ =='__main__':
     app.run(debug=True)
