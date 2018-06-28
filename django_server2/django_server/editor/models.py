936a46623be23d125ff74607cc061e7c65188a6e
Kim Sangyeon <dho113@naver.com>
2018-06-28 22:32:47 +0900

    django_server 제거

--- django_server2/django_server/editor/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Document(models.Model):
-    editor = models.TextField()
-    title = models.CharField(max_length=200)
-    html = models.TextField()
-    css = models.TextField()
